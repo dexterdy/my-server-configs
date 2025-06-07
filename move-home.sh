78dcb3c48fb4a963fd062bbfbcd029e3d8fe1325
Dexterdy <j.p.hagedoorn@proton.me>
2025-06-07 23:09:53 +0200

    adjustment

--- move-home.sh
+++ move-home.sh
@@ -1,7 +1,7 @@
 for dir in *; do
     if [ -d $dir ]; then
         mkdir -p "home/$dir/$dir-app"
-        /bin/cp -rf "$dir" "/home/$dir/$dir-app"
+        /bin/cp -rf "$dir/*" "/home/$dir/$dir-app"
         chown -R $dir:$dir "/home/$dir/$dir-app"
     fi
 done
