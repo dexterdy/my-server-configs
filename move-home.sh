6cd8038b89657824c7e213d14d4c94528cd63af5
Dexterdy <j.p.hagedoorn@proton.me>
2025-06-07 22:43:02 +0200

    syntax?

--- move-home.sh
+++ move-home.sh
@@ -1,4 +1,4 @@
 for dir in */; do
-    /bin/cp -rf "$dir/*" "/home/$dir/$dir-app"
-    chown -R $dir:$dir "/home/$dir/$dir-app"
+    /bin/cp -rf "$dir/*" "/home/$dir/$dir-app";
+    chown -R $dir:$dir "/home/$dir/$dir-app";
 end
